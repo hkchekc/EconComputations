871ec5d3e971ac4c5a8f1af314c16a41c6a54462
Chek Yin Choi <chekchek2007@gmail.com>
2019-12-11 17:08:38 -0600

    Modules for ps8

--- test.f95
+++ test.f95
@@ -11,12 +11,15 @@ program test
         real:: at = 20
         integer:: i  ! looping idx
         test_array = (/(1.0/2.0**i, i = 1, 10)/)
-        print*, test_array
         IMIT_ARRAY = TEST_ARRAY
         PRINT*,"imit", IMIT_ARRAY
+        print*, "test", test_array
+        test_array(:)=0.
+        print*, "with brac", test_array
+        test_array = 0.
+        print*, "no brac", test_array
         CALL DLLM(AT, 10.)
         CALL CHANGE_PARA()
-        print*, "main:", exp 
 end program test
 
 subroutine dllm(t, Y)
@@ -25,16 +28,12 @@ subroutine dllm(t, Y)
         real, INTENT(IN):: y
         real:: t
         t = t+1*y
-        print*, "dllm:ori", exp
         EXP = 100
-        PRINT*, EXP
 end subroutine
 
 SUBROUTINE CHANGE_PARA()
         USE TESTPARA
-        PRINT*,"change para:", EXP
         exp = 2000.
-        print*, "post change", exp
 END SUBROUTINE
 
 
