53a0c5907efc33f03e3164dd7cdd3a708bcbe158
Chek Yin Choi <chekchek2007@gmail.com>
2020-01-13 10:14:23 -0600

    Finish ps7

--- ps7.f95
+++ ps7.f95
@@ -1,28 +1,29 @@
 MODULE PS7PARA
     IMPLICIT NONE
-    REAL(KIND=8), PARAMETER:: BETA=0.96, SIGMA=1, ALPHA=0.33, DELTA=0.06
-    INTEGER, PARAMETER:: NK=40, NKBAR=10
+    REAL(KIND=8), PARAMETER:: BETA=0.99, SIGMA=1, ALPHA=0.36
+    INTEGER, PARAMETER:: NK=40, NAK=40
     INTEGER:: I
-    REAL(KIND=8), PARAMETER:: K_MIN=0.01, K_MAX=10., STEP=(10.-0.01)/40.
+    REAL(KIND=8), PARAMETER:: K_MIN=0.01, K_MAX=0.5, STEP=(0.5-0.01)/FLOAT(NK-1)
     REAL(KIND=8), DIMENSION(NK), PARAMETER:: K_GRID=(/(I*STEP, I=1, NK)/) +K_MIN - STEP
-    REAL(KIND=8), PARAMETER:: KBAR_MIN=0.01, KBAR_MAX=10., BAR_STEP=4./9.
-    REAL(KIND=8), DIMENSION(NK), PARAMETER:: KBAR_GRID=(/(I*BAR_STEP, I=1, NKBAR)/) +KBAR_MIN - BAR_STEP
-    REAL(KIND=8):: A0=0.05, A1=0.9
+    REAL(KIND=8), PARAMETER:: KBAR_MIN=0.15, KBAR_MAX=0.25, BAR_STEP=0.1/FLOAT(NAK-1)
+    REAL(KIND=8), DIMENSION(NAK), PARAMETER:: AK_GRID=(/(I*BAR_STEP, I=1, NAK)/) +KBAR_MIN - BAR_STEP
+    REAL(KIND=8):: A0=0.01, A1=0.99
+    REAL(KIND=8), PARAMETER:: CRIT=1E-3
 END MODULE
 
 MODULE PS7RES
-    PS7PARA
+    USE PS7PARA
     IMPLICIT NONE
-    REAL(KIND=8), DIMENSION(NKBAR):: INTEREST, WAGE
-    REAL(KIND=8), DIMENSION(NK):: K2BAR
-    REAL(KIND=8), DIMENSION(NK):: UTIL=EXP(K_GRID)
-    REAL(KIND=8), DIMENSION(NK):: B,C,D
+    REAL(KIND=8), DIMENSION(NAK):: INTEREST, WAGE
+    INTEGER, DIMENSION(NK, NAK):: PFUNC
+    REAL(KIND=8), DIMENSION(NK, NAK):: VFUNC,  VFUNC_NEW
+    REAL(KIND=8), DIMENSION(NK):: KPR_EST
+    REAL(KIND=8), DIMENSION(NK, NAK, NK):: UTIL, CONSUM
     CONTAINS
         SUBROUTINE INIT_RW()
-            DO I=1, NKBAR
-                INTEREST(I)= ALPHA*(KBAR_GRID(I)**(ALPHA-1))-DELTA
-                WAGE(I)= (1-ALPHA)*(KBAR_GRID(I)**ALPHA)
-                K2BAR(I) = EXP(A0+A1*LOG(KBAR_GRID(I)))
+            DO I=1, NAK
+                INTEREST(I)= ALPHA*(AK_GRID(I)**(ALPHA-1.))
+                WAGE(I)= (1.-ALPHA)*(AK_GRID(I)**ALPHA)
             ENDDO
         END SUBROUTINE
 END MODULE
@@ -30,171 +31,344 @@ END MODULE
 PROGRAM PS7
     USE PS7PARA
     USE PS7RES
+    USE OMP_LIB
     IMPLICIT NONE
-    REAL(KIND=8), DIMENSION(NKBAR):: INTEREST, WAGE
-    INTEGER:: KBIDX, KIDX
+    INTEGER:: KIDX, AKIDX, KPIDX
+    REAL(KIND=8):: ERROR_VFI
 
-    CALL SPLINE(K_GRID, K2BAR)
-    DO KBIDX=1, NKBAR
-        DO KIDX=1, NK
-            CALL ISPLINE(KBIDX, KIDX)
+        CALL INIT_RW()
+
+        ! FOR PARALLEL CALCULATION, REDUCE PRIVATE VARIABLES IN MAIN LOOP
+        ! PRE CALCULATE THE KPR, CONSUM, UTILTIY
+        DO AKIDX=1,NAK
+            KPR_EST(AKIDX) = AK_GRID(AKIDX) ! NO MOTION
+            PRINT*, KPR_EST(AKIDX)
         ENDDO
+
+        UTIL = -1e12
+        !$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(AKIDX, KIDX, KPIDX)
+        DO AKIDX=1,NAK
+        DO KIDX=1,NK
+        DO KPIDX=1,NK ! NEXT PERIOD K CHOICE
+            CONSUM(KIDX, AKIDX, KPIDX) = (INTEREST(AKIDX))*K_GRID(KIDX)&
+            +WAGE(AKIDX)-K_GRID(KPIDX)
+            IF (CONSUM(KIDX, AKIDX, KPIDX)>0.) THEN
+                UTIL(KIDX, AKIDX, KPIDX)= LOG(CONSUM(KIDX, AKIDX, KPIDX))
+            ENDIF
+        ENDDO
+        ENDDO
+        ENDDO
+        !$OMP END PARALLEL DO
+
+    ERROR_VFI = 100.
+    DO AKIDX=1,NAK ! INIT VALUE
+    DO KIDX=1,NK
+        VFUNC(KIDX, AKIDX)= MAXVAL(UTIL(KIDX, AKIDX, :),1)
     ENDDO
-        
+    ENDDO
+    DO WHILE (ERROR_VFI > CRIT)
+        CALL VFI()
+        ERROR_VFI = MAXVAL(ABS(VFUNC-VFUNC_NEW))
+        VFUNC = VFUNC_NEW
     ENDDO
 
+    CALL WRITE_ALL()
 END PROGRAM PS7
-! COPYING SOMEONE'S CODE
-subroutine spline (X,Y)
-!======================================================================
-!  Calculate the coefficients b(i), c(i), and d(i), i = 1, 2, ...,n
-!  for cubic spline interpolation
-!  s(x) = y(i) + b(i)*(x-x(i)) + c(i)*(x-x(i))**2+d(i)*(x-x(i))**3
-!  for  x(i) <= x <= x(i+1)
-!  Alex G: January 2010
-!----------------------------------------------------------------------
-!  input..
-!  x = the arrays of data abscissas (in strictly increasing order)
-!  y = the arrays of data ordinates
-!  n = size of the arrays xi() and yi() (n >= 2)
-!  output..
-!  b, c, d  = arrays of spline coefficients
-!  comments ...
-!  spline.f90 program is based on fortran version of program spline.f
-!  the accompanying function fspline can be used for interpolation
-!======================================================================
+
+SUBROUTINE VFI()
     USE PS7PARA
     USE PS7RES
-    implicit none
-    double precision x(n), y(n)
-    integer i, j, gap
-    double precision h
-
-    gap = n-1
-    ! check input
-    if ( n < 2 ) return
-    if ( n < 3 ) then
-      b(1) = (y(2)-y(1))/(x(2)-x(1))   ! linear interpolation
-      c(1) = 0.
-      d(1) = 0.
-      b(2) = b(1)
-      c(2) = 0.
-      d(2) = 0.
-      return
-    end if
-    !
-    ! step 1: preparation
-    !
-    d(1) = x(2) - x(1)
-    c(2) = (y(2) - y(1))/d(1)
-    do i = 2, gap
-      d(i) = x(i+1) - x(i)
-      b(i) = 2.0*(d(i-1) + d(i))
-      c(i+1) = (y(i+1) - y(i))/d(i)
-      c(i) = c(i+1) - c(i)
-    end do
-    !
-    ! step 2: end conditions
-    !
-    b(1) = -d(1)
-    b(n) = -d(n-1)
-    c(1) = 0.0
-    c(n) = 0.0
-    if(n /= 3) then
-      c(1) = c(3)/(x(4)-x(2)) - c(2)/(x(3)-x(1))
-      c(n) = c(n-1)/(x(n)-x(n-2)) - c(n-2)/(x(n-1)-x(n-3))
-      c(1) = c(1)*d(1)**2/(x(4)-x(1))
-      c(n) = -c(n)*d(n-1)**2/(x(n)-x(n-3))
-    end if
-    !
-    ! step 3: forward elimination
-    !
-    do i = 2, n
-      h = d(i-1)/b(i-1)
-      b(i) = b(i) - h*d(i-1)
-      c(i) = c(i) - h*c(i-1)
-    end do
-    !
-    ! step 4: back substitution
-    !
-    c(n) = c(n)/b(n)
-    do j = 1, gap
-      i = n-j
-      c(i) = (c(i) - d(i)*c(i+1))/b(i)
-    end do
-    !
-    ! step 5: compute spline coefficients
-    !
-    b(n) = (y(n) - y(gap))/d(gap) + d(gap)*(c(gap) + 2.0*c(n))
-    do i = 1, gap
-      b(i) = (y(i+1) - y(i))/d(i) - d(i)*(c(i+1) + 2.0*c(i))
-      d(i) = (c(i+1) - c(i))/d(i)
-      c(i) = 3.*c(i)
-    end do
-    c(n) = 3.0*c(n)
-    d(n) = d(n-1)
-end subroutine spline
-
- function ispline(u)
-!======================================================================
-! function ispline evaluates the cubic spline interpolation at point z
-! ispline = y(i)+b(i)*(u-x(i))+c(i)*(u-x(i))**2+d(i)*(u-x(i))**3
-! where  x(i) <= u <= x(i+1)
-!----------------------------------------------------------------------
-! input..
-! u       = the abscissa at which the spline is to be evaluated
-! x, y    = the arrays of given data points ! kbar and k in this context
-! b, c, d = arrays of spline coefficients computed by spline ! in modules
-! n       = the number of data points !
-! output:
-! ispline = interpolated value at point u
-! Same Author as above
-!=======================================================================
-implicit none
-double precision ispline
-integer n
-double precision  u
-integer i, j, k
-double precision dx
-
-! if u is ouside the x() interval take a boundary value (left or right)
-if(u <= x(1)) then
-  ispline = y(1)
-  return
-end if
-if(u >= x(n)) then
-  ispline = y(n)
-  return
-end if
-
-!*
-!  binary search for for i, such that x(i) <= u <= x(i+1)
-!*
-i = 1
-j = n+1
-do while (j > i+1)
-  k = (i+j)/2
-  if(u < x(k)) then
-        j = k
-    else
-        i = k
-   end if
-end do
-!*
-!  evaluate spline interpolation
-!*
-dx = u-x(i)
-ispline = y(i) + dx*(b(i) + dx*(c(i) + dx*d(i)))
-end function ispline
-
-
-SUBROUTINE MYPLINE(KBIDX, KIDX)
-    USE PS7PARA
-    USE PS7RES
+    USE OMP_LIB
     IMPLICIT NONE
-    INTEGER, INTENT(IN):: KBIDX, KIDX
-    REAL(KIND=8):: CONSUM
+    INTEGER:: AKIDX, KIDX, KPIDX
+    REAL(KIND=8), DIMENSION(1):: NEXTU, KPR, SMALL_KPR
+    REAL(KIND=8), DIMENSION(NK, NAK, NK):: VFUNC_TMP
 
-    CONSUM = (1+INTEREST(KBIDX))*K_GRID(KIDX) + WAGE(KIDX) -
+
+    !$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(AKIDX, KIDX, KPIDX)
+    DO AKIDX=1,NAK
+        KPR(1) = KPR_EST(AKIDX)
+    DO KIDX=1,NK
+    DO KPIDX=1,NK ! NEXT PERIOD K CHOICE
+        SMALL_KPR(1) = K_GRID(KPIDX)
+        CALL PWL_INTERP_2D(NK, NAK, K_GRID, AK_GRID, VFUNC, 1, SMALL_KPR, KPR, NEXTU)
+        IF (NEXTU(1)>1E10 .OR. -1E10>NEXTU(1)) THEN
+            PRINT*, SMALL_KPR(1),KPR(1), AK_GRID(NAK), K_GRID(NK),AK_GRID(1), K_GRID(1)
+        ENDIF
+        VFUNC_TMP(KIDX, AKIDX, KPIDX) = UTIL(KIDX, AKIDX, KPIDX)+BETA*NEXTU(1)
+    ENDDO
+        VFUNC_NEW(KIDX, AKIDX) = MAXVAL(VFUNC_TMP(KIDX, AKIDX, :),1)
+        PFUNC(KIDX,AKIDX) = MAXLOC(VFUNC_TMP(KIDX, AKIDX, :),1)
+    ENDDO
+    ENDDO
+    !$OMP END PARALLEL DO
+END SUBROUTINE
+
+subroutine pwl_interp_2d ( nxd, nyd, xd, yd, zd, ni, xi, yi, zi )
+
+!*****************************************************************************80
+!
+!! PWL_INTERP_2D: piecewise linear interpolant to data defined on a 2D grid.
+!
+!  Discussion:
+!
+!    Thanks to Adam Hirst for pointing out an error in the formula that
+!    chooses the interpolation triangle, 04 February 2018.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    04 February 2018
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) NXD, NYD, the number of X and Y data values.
+!
+!    Input, real ( kind = 8 ) XD(NXD), YD(NYD), the sorted X and Y data.
+!
+!    Input, real ( kind = 8 ) ZD(NXD,NYD), the Z data.
+!
+!    Input, integer ( kind = 4 ) NI, the number of interpolation points.
+!
+!    Input, real ( kind = 8 ) XI(NI), YI(NI), the coordinates of the
+!    interpolation points.
+!
+!    Output, real ( kind = 8 ) ZI(NI), the value of the interpolant.
+!
+  implicit none
+
+  integer ( kind = 4 ):: ni
+  integer ( kind = 4 ):: nxd
+  integer ( kind = 4 ):: nyd
+
+  real ( kind = 8 ):: alpha1
+  real ( kind = 8 ):: beta1
+  real ( kind = 8 ):: det
+  real ( kind = 8 ):: dxa
+  real ( kind = 8 ):: dxb
+  real ( kind = 8 ):: dxi
+  real ( kind = 8 ):: dya
+  real ( kind = 8 ):: dyb
+  real ( kind = 8 ):: dyi
+  real ( kind = 8 ):: gamma
+  integer ( kind = 4 ):: i1
+  integer ( kind = 4 ):: j
+  integer ( kind = 4 ):: k
+  real ( kind = 8 ):: r8_huge= 1.79769313486231571D+308
+  integer ( kind = 4 ):: r8vec_bracket5
+  real ( kind = 8 ):: xd(nxd)
+  real ( kind = 8 ):: xi(ni)
+  real ( kind = 8 ):: yd(nyd)
+  real ( kind = 8 ):: yi(ni)
+  real ( kind = 8 ):: zd(nxd,nyd)
+  real ( kind = 8 ):: zi(ni)
+
+  do k = 1, ni
+!
+!  For interpolation point (xi(k),yi(k)), find data intervals I and J so that:
+!
+!    xd(i) <= xi(k) <= xd(i+1),
+!    yd(j) <= yi(k) <= yd(j+1).
+!
+!  But if the interpolation point is not within a data interval,
+!  assign the dummy interpolant value zi(k) = infinity.
+!
+    i1 = r8vec_bracket5 ( nxd, xd, xi(k) )
+    if ( i1 == -1 ) then
+      zi(k) = r8_huge
+      cycle
+    end if
+
+    j = r8vec_bracket5 ( nyd, yd, yi(k) )
+    if ( j == -1 ) then
+      zi(k) = r8_huge
+      cycle
+    end if
+!
+!  The rectangular cell is arbitrarily split into two triangles.
+!  The linear interpolation formula depends on which triangle
+!  contains the data point.
+!
+!    (I,J+1)--(I+1,J+1)
+!      |\       |
+!      | \      |
+!      |  \     |
+!      |   \    |
+!      |    \   |
+!      |     \  |
+!    (I,J)---(I+1,J)
+!
+    if ( yi(k) < yd(j+1) &
+      + ( yd(j) - yd(j+1) ) * ( xi(k) - xd(i1) ) / ( xd(i1+1) - xd(i1) ) ) then
+
+      dxa = xd(i1+1) - xd(i1)
+      dya = yd(j)   - yd(j)
+
+      dxb = xd(i1)   - xd(i1)
+      dyb = yd(j+1) - yd(j)
+
+      dxi = xi(k)   - xd(i1)
+      dyi = yi(k)   - yd(j)
+
+      det = dxa * dyb - dya * dxb
+
+      alpha1 = ( dxi * dyb - dyi * dxb ) / det
+      beta1 =  ( dxa * dyi - dya * dxi ) / det
+      gamma = 1.0D+00 - alpha1 - beta1
+
+      zi(k) = alpha1 * zd(i1+1,j) + beta1 * zd(i1,j+1) + gamma * zd(i1,j)
+
+    else
+
+      dxa = xd(i1)   - xd(i1+1)
+      dya = yd(j+1) - yd(j+1)
+
+      dxb = xd(i1+1) - xd(i1+1)
+      dyb = yd(j)   - yd(j+1)
+
+      dxi = xi(k)   - xd(i1+1)
+      dyi = yi(k)   - yd(j+1)
+
+      det = dxa * dyb - dya * dxb
+
+      alpha1 = ( dxi * dyb - dyi * dxb ) / det
+      beta1 =  ( dxa * dyi - dya * dxi ) / det
+      gamma = 1.0D+00 - alpha1 - beta1
+
+      zi(k) = alpha1 * zd(i1,j+1) + beta1 * zd(i1+1,j) + gamma * zd(i1+1,j+1)
+
+    end if
+  end do
+end subroutine
+
+function r8vec_bracket5 ( nd, xd, xi )
+
+!*****************************************************************************80
+!
+!! R8VEC_BRACKET5 brackets data between successive entries of a sorted R8VEC.
+!
+!  Discussion:
+!
+!    We assume XD is sorted.
+!
+!    If XI is contained in the interval [XD(1),XD(N)], then the returned
+!    value B indicates that XI is contained in [ XD(B), XD(B+1) ].
+!
+!    If XI is not contained in the interval [XD(1),XD(N)], then B = -1.
+!
+!    This code implements a version of binary search which is perhaps more
+!    understandable than the usual ones.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    14 October 2012
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) ND, the number of data values.
+!
+!    Input, real ( kind = 8 ) XD(N), the sorted data.
+!
+!    Input, real ( kind = 8 ) XD, the query value.
+!
+!    Output, integer ( kind = 4 ) R8VEC_BRACKET5, the bracket information.
+!
+  implicit none
+
+  integer ( kind = 4 ) nd
+
+  integer ( kind = 4 ) b
+  integer ( kind = 4 ) l
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) r
+  integer ( kind = 4 ) r8vec_bracket5
+  real ( kind = 8 ) xd(nd)
+  real ( kind = 8 ) xi
+
+  if ( xi < xd(1) .or. xd(nd) < xi ) then
+
+    b = -1
+
+  else
+
+    l = 1
+    r = nd
+
+    do while ( l + 1 < r )
+      m = ( l + r ) / 2
+      if ( xi < xd(m) ) then
+        r = m
+      else
+        l = m
+      end if
+    end do
+
+    b = l
+
+  end if
+
+  r8vec_bracket5 = b
+
+  return
+end
+
+SUBROUTINE WRITE_ALL()
+    USE PS7RES
+    USE PS7PARA
+    IMPLICIT NONE
+    INTEGER:: SROWIDX
+    CHARACTER(LEN=130):: PATH="/Users/chek_choi/Downloads/fortran/"
+    CHARACTER(LEN=150):: FILE_NAME
+    REAL(KIND=8), DIMENSION(1):: NEXTU, SMALL_KPR
+
+        FILE_NAME = TRIM(PATH)//"VFUNC"
+        OPEN(UNIT=1, FILE=FILE_NAME, STATUS='REPLACE') ! START WITH THE TWO VALUE FUNCTIONS
+        DO SROWIDX=1, NAK
+            WRITE(UNIT=1,FMT=*) VFUNC(16,SROWIDX)
+        ENDDO
+        CLOSE(UNIT=1)
+
+        FILE_NAME = TRIM(PATH)//"VFUNC_SS"
+        SMALL_KPR(1) = 0.1995
+        OPEN(UNIT=3, FILE=FILE_NAME, STATUS='REPLACE') ! START WITH THE TWO VALUE FUNCTIONS
+        DO SROWIDX=1, NAK
+            CALL PWL_INTERP_2D(NK, NAK, K_GRID, AK_GRID, VFUNC, 1, SMALL_KPR, AK_GRID(SROWIDX), NEXTU)
+                IF (NEXTU(1)> 1E12) THEN
+                    PRINT*, AK_GRID(SROWIDX), K_GRID(NK), AK_GRID(NAK)
+                ENDIF
+            WRITE(UNIT=3,FMT=*) NEXTU(1)
+        ENDDO
+        CLOSE(UNIT=3)
+
+        FILE_NAME = TRIM(PATH)//"PFUNC"
+        OPEN(UNIT=2, FILE=FILE_NAME, STATUS='REPLACE') ! ALSO SAVE POLICY FUNCTIONS
+        DO SROWIDX=1, NK
+            WRITE(UNIT=2,FMT=*) K_GRID(PFUNC(SROWIDX,NAK/2))
+        ENDDO
+        CLOSE(UNIT=2)
+
+        FILE_NAME = TRIM(PATH)//"KGRID"
+        OPEN(UNIT=4, FILE=FILE_NAME, STATUS='REPLACE') ! ALSO SAVE POLICY FUNCTIONS
+        DO SROWIDX=1, NAK
+            WRITE(UNIT=4,FMT=*)   K_GRID(SROWIDX)
+        ENDDO
+        CLOSE(UNIT=4)
+
 
 END SUBROUTINE
+
